bbc15ab9e4a62b8f022ee88ca7783155a667ddc6
Himanshu yadav <78066262+Moriarity5833@users.noreply.github.com>
2023-11-28 12:06:58 +0530

    just debuggung

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,8 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client("postgres://ukksnjfg:zUrkVMYx3WswwQJjTgzPBtuf8rtdHzUi@silly.db.elephantsql.com/ukksnjfg");
     await client.connect();
+    console.log(client)
     return client;
 }
